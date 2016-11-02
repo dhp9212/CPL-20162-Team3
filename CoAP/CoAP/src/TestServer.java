bc89dae3c6eaa6c63b979f0fbcfe619f690135f4
dhp9212 <dhp9212@gmail.com>
2016-11-03 05:27:40 +0900

    Revert "DB test 추가"
    This reverts commit e8df7c3f0b27c9a3f46c478456497e06c88cff38.

--- CoAP/CoAP/src/TestServer.java
+++ CoAP/CoAP/src/TestServer.java
@@ -30,8 +30,6 @@ public class TestServer extends CoapServer{
 		catch(SocketException e){
 			System.err.println("Failed to initialize server : " + e.getMessage());
 		}
-		
-		
 	}
 	
 	private void addEndpoints(){
@@ -62,14 +60,11 @@ public class TestServer extends CoapServer{
 			getAttributes().setTitle("CoAP Server Resource");
 		}
 		
-		
-		// If server receives GET from client
 		@Override
 		public void handleGET(CoapExchange ex){
 			ex.respond(message);
 		}
 		
-		// If server receives PUT from client
 		@Override
 		public void handlePUT(CoapExchange exchange){
 			byte[] payload = exchange.getRequestPayload();
